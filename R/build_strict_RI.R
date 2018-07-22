3d76d4c6bf1709e8f302853f06d10c1816d18c72
sl809869069 <809869069@qq.com>
2018-07-22 20:56:27 +0800

    alter "build_strict_ri.R" if(count<100){ri=max(x)}

--- R/build_strict_RI.R
+++ R/build_strict_RI.R
@@ -1,13 +1,17 @@
 build_strict_ri <- function(male,female,upper.ri = 0.975){
   male_ri=c()
   for(i in 1:nrow(male)){
-    ri=quantile(male[i,][which(as.numeric(male[i,])>0)],upper.ri,na.rm = T)
+    count=apply(male[i,],1,function(x){return(length(x[x>0]))})
+    if(count<100){ri=max(male[i,])
+    }else{ri=quantile(male[i,][which(as.numeric(male[i,])>0)],upper.ri,na.rm = T)}
     ri=data.frame(genesymbol=row.names(male[i,]),ri)
     male_ri=rbind(male_ri,ri)
   }
   female_ri=c()
   for(i in 1:nrow(female)){
-    ri=quantile(female[i,][which(as.numeric(female[i,])>0)],upper.ri,na.rm = T)
+    count=apply(female[i,],1,function(x){return(length(x[x>0]))})
+    if(count<100){ri=max(female[i,])
+    }else{ri=quantile(female[i,][which(as.numeric(female[i,])>0)],upper.ri,na.rm = T)}
     ri=data.frame(genesymbol=row.names(female[i,]),ri)
     female_ri=rbind(female_ri,ri)
   }
@@ -18,3 +22,16 @@ build_strict_ri <- function(male,female,upper.ri = 0.975){
   ri = round(ri,2)
   return(ri)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
