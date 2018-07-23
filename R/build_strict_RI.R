e261f71174e1537d360ca73142d1aa91b895214b
sl809869069 <809869069@qq.com>
2018-07-23 21:06:36 +0800

    recalculate SRI

--- R/build_strict_RI.R
+++ R/build_strict_RI.R
@@ -1,17 +1,42 @@
+# load('Normal_male_Top1000.RData')
+
 build_strict_ri <- function(male,female,upper.ri = 0.975){
   male_ri=c()
   for(i in 1:nrow(male)){
+    ifot=as.numeric(male[i,])
     count=apply(male[i,],1,function(x){return(length(x[x>0]))})
-    if(count<100){ri=max(male[i,])
-    }else{ri=quantile(male[i,][which(as.numeric(male[i,])>0)],upper.ri,na.rm = T)}
+    if(count<10){
+      ri=0
+    }else if(count>=10 & count<120){
+      if(upper.ri<0.975){
+        ri=quantile(ifot[which(ifot>0)],upper.ri)
+        }
+      else{
+        ri=max(male[i,])
+        }
+    }else{
+      ri=quantile(ifot[which(ifot>0)],upper.ri)
+    }
     ri=data.frame(genesymbol=row.names(male[i,]),ri)
     male_ri=rbind(male_ri,ri)
   }
+  
   female_ri=c()
   for(i in 1:nrow(female)){
+    ifot=as.numeric(female[i,])
     count=apply(female[i,],1,function(x){return(length(x[x>0]))})
-    if(count<100){ri=max(female[i,])
-    }else{ri=quantile(female[i,][which(as.numeric(female[i,])>0)],upper.ri,na.rm = T)}
+    if(count<10){
+      ri=0
+    }else if(count>=10 & count<120){
+      if(upper.ri<0.975){
+        ri=quantile(ifot[which(ifot>0)],upper.ri)
+      }
+      else{
+        ri=max(female[i,])
+      }
+    }else{
+      ri=quantile(ifot[which(ifot>0)],upper.ri)
+    }    
     ri=data.frame(genesymbol=row.names(female[i,]),ri)
     female_ri=rbind(female_ri,ri)
   }
